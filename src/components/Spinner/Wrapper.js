251f60ae50445fe7b92a3cc89e528ab7456e2d50
Rodrigo Ferreira <rodrigofepy@gmail.com>
2017-12-05 23:29:35 -0300

    Add Spinner component

---
+++ src/components/Spinner/Wrapper.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+import { colors } from '../../styles'
+
+const Wrapper = styled.div`
+  align-items: center;
+  flex-grow: 1;
+  justify-content: center;
+
+  display: flex;
+
+  width: 100%;
+
+  background-color: ${colors.lightestGrey};
+`
+
+export default Wrapper
